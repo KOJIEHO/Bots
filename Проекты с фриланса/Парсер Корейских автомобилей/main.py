e719abd74198416823ec6370c43edbc65a2bf09d
KOJIEHO <75936661+KOJIEHO@users.noreply.github.com>
2023-12-04 21:12:56 +0300

    Update main.py

--- Проекты с фриланса/Парсер Корейских автомобилей/main.py
+++ Проекты с фриланса/Парсер Корейских автомобилей/main.py
@@ -9,7 +9,8 @@ headers = {
                   "Chrome/98.0.4758.119 YaBrowser/22.3.0.2430 Yowser/2.5 Safari/537.36",
     "accept": "*/*"}
 url = ['http://api.encar.com/search/car/list/premium?count=true&q=(And.Year.range(201800..202299)._.Mileage.range(10000..70000)._.Hidden.N._.CarType.Y._.Trust.Warranty._.Condition.Inspection._.(Or.FuelType.%EB%94%94%EC%A0%A4._.FuelType.%EA%B0%80%EC%86%94%EB%A6%B0.)_.Transmission.%EC%98%A4%ED%86%A0.)&sr=%7CModifiedDate%7C0%7C50', 'http://api.encar.com/search/car/list/premium?count=true&q=(And.Year.range(201800..202299)._.Mileage.range(10000..70000)._.Hidden.N._.CarType.N._.Trust.Warranty._.Condition.Inspection._.(Or.FuelType.%EB%94%94%EC%A0%A4._.FuelType.%EA%B0%80%EC%86%94%EB%A6%B0.)_.Transmission.%EC%98%A4%ED%86%A0.)&sr=%7CModifiedDate%7C0%7C50']
-bot = Bot('5644402304:AAHdwG-BJE9ImPiO4RI4nGdxe0jGvU5C7vA')
+token = ''
+bot = Bot(token)
 db = Dispatcher(bot)
 
 
